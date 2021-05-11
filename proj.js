db1f1dc78dcfd308612d2fa89c26e14ffa3a291e
pickj888 <sebastianjpwhite@gmail.com>
2021-05-11 16:19:47 +1200

    Added moving lines
    Added the moving lines that will be used later in the game

--- proj.js
+++ proj.js
@@ -11,9 +11,13 @@ console.log("Game")
 // Some constants that will be used in this script
 const WIDTH = 600
 const HEIGHT = 600
+const LINE_SIZE = 10
+
 
 // Some variables that will be used in this script
 var ctx
+var linesLocation = -4
+var linesSpeed = 2
 
 window.onload=startCanvas
 
@@ -36,4 +40,17 @@ function updateCanvas(){
 	ctx.fillStyle="white"
 	ctx.fillRect(0,0,WIDTH, HEIGHT)
 	
+	// Draw the two main lines
+	ctx.fillStyle="black"
+	ctx.fillRect(0,linesLocation,HEIGHT,LINE_SIZE)
+	ctx.fillRect(linesLocation, 0, LINE_SIZE, WIDTH)
+	linesLocation = linesLocation + linesSpeed
+
+	// changes the line speed when the lines hit edge of the canvas
+	if (linesLocation > HEIGHT-5 || linesLocation < -5) {
+		linesSpeed = linesSpeed*-1
+	}
+	// if (lineLocation < -5) {
+	// 	lineSpeed = 2
+	// }
 }
